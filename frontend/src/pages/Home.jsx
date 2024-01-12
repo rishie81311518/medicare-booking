78c62129d0b18fdf1ee81a5724d1147aa41abed4
Rishi <rishiekandala@gmail.com>
2024-01-12 12:23:18 +0530

    Testimonial

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -15,6 +15,7 @@ import About from '../components/About/About'
 import ServiceList from '../components/Services/ServiceList'
 import DoctorList from '../components/Doctors/DoctorList'
 import FaqList from '../components/Faq/FaqList'
+import Testimonial from '../components/Testimonial/Testimonial'
 
 const Home = () => {
 return (
@@ -257,6 +258,7 @@ World-class care for everyone. Our health System offers unmatched, expert health
 World-class care for everyone. Our health System offers unmatched, expert health care.
 </p>
 </div>
+
 <DoctorList/>
 
     </div>
@@ -284,6 +286,23 @@ World-class care for everyone. Our health System offers unmatched, expert health
 </section>
 {/* ===== faq section end ===== */}
 
+{/* ===== testimonial ===== */}
+<section>
+    <div className='container'>
+    <div className='xl:w-[470px] mx-auto'>
+<h2 className='heading text-center'>What our patient say</h2>
+<p className='text__para text-center'>
+World-class care for everyone. Our health System offers unmatched, expert health care.
+</p>
+</div>
+
+<Testimonial />
+
+</div>
+
+</section>
+
+{/* ===== testimonial end ===== */}
 
 
 
